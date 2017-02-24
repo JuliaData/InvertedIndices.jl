71eb84044ab2cb2bcee212bae05ea709c07a92fa
Matt Bauman <mbauman@gmail.com>
2017-02-24 10:40:44 -0500

    initial implementation

--- src/InvertedIndices.jl
+++ src/InvertedIndices.jl
@@ -1,5 +1,16 @@
 module InvertedIndices
 
-# package code goes here
+export InvertedIndex, Not
+
+struct InvertedIndex{T}
+    skip::T
+end
+const Not = InvertedIndex
+
+@inline function Base.to_indices(A, inds, I::Tuple{InvertedIndex, Vararg{Any}})
+    v = trues(Base.uncolon(inds, (:, Base.tail(I)...)))
+    v[I[1].skip] = false
+    to_indices(A, inds, (v, Base.tail(I)...))
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,21 @@
 using InvertedIndices
 using Base.Test
 
-# write your own tests here
-@test 1 == 2
+@testset "1-d readonly" for A in (-10:13,)
+    @test A[Not(1)] == A[Not(1:1)] == A[Not(A.==-10)] == collect(-9:13)
+    @test @views A[Not(1)] == A[Not(1:1)] == A[Not(A.==-10)] == collect(-9:13)
+    @test A[Not(end)] == A[Not(end:end)] == A[Not(A.==13)] == collect(-10:12)
+    @test @views A[Not(end)] == A[Not(end:end)] == A[Not(A.==13)] == collect(-10:12)
+    @test A[Not(iseven.(A))] == A[isodd.(A)]
+    @test A[Not([])] == A[collect(1:end)] == collect(A)
+end
+
+@testset "vector array" begin
+    A = collect(1:4)
+    A[Not(2:3)] = [44, 11]
+    @test A == [44, 2, 3, 11]
+    A[Not(2:4)] = 0
+    @test A == [0, 2, 3, 11]
+    A[Not([])] = 0
+    @test all(A.== 0)
+end

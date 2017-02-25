52587a89515beb6b47edbe51d57c5d2829127ac3
Matt Bauman <mbauman@gmail.com>
2017-02-24 19:24:09 -0500

    support boolean masks

--- src/InvertedIndices.jl
+++ src/InvertedIndices.jl
@@ -7,10 +7,23 @@ struct InvertedIndex{T}
 end
 const Not = InvertedIndex
 
+# nindices(::InvertedIndex) = 1
+# nindices{N}(::InvertedIndex{<:AbstractArray{Bool,N}}}) = N
+# nindices{N}(::InvertedIndex{<:AbstractArray{CartesianIndex{N}}}}) = N
+
+@inline trues(tup) = Base.trues(map(Base.unsafe_length, tup))
+
 @inline function Base.to_indices(A, inds, I::Tuple{InvertedIndex, Vararg{Any}})
-    v = trues(Base.uncolon(inds, (:, Base.tail(I)...)))
+    v = trues(spanned_indices(inds, I))
     v[I[1].skip] = false
     to_indices(A, inds, (v, Base.tail(I)...))
+end
+
+@inline spanned_indices(inds, I::Tuple{InvertedIndex,Vararg{Any}}) = (Base.uncolon(inds, (:, Base.tail(I)...)),)
+@inline spanned_indices(inds, I::Tuple{InvertedIndex{<:AbstractVector},Vararg{Any}}) = (Base.uncolon(inds, (:, Base.tail(I)...)),)
+@inline function spanned_indices{N}(inds, I::Tuple{InvertedIndex{<:AbstractArray{Bool,N}},Vararg{Any}})
+    heads, tails = Base.IteratorsMD.split(inds, Val{N})
+    (Base.front(heads)..., Base.uncolon((heads[end], tails...), (:, Base.tail(I)...)))
 end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,13 +1,13 @@
 using InvertedIndices
 using Base.Test
 
-@testset "1-d readonly" for A in (-10:13,)
+@testset "1-d readonly" for A in (-10:13, reshape(-10:13,2,:), reshape(-10:13,3,2,:))
     @test A[Not(1)] == A[Not(1:1)] == A[Not(A.==-10)] == collect(-9:13)
     @test @views A[Not(1)] == A[Not(1:1)] == A[Not(A.==-10)] == collect(-9:13)
     @test A[Not(end)] == A[Not(end:end)] == A[Not(A.==13)] == collect(-10:12)
     @test @views A[Not(end)] == A[Not(end:end)] == A[Not(A.==13)] == collect(-10:12)
     @test A[Not(iseven.(A))] == A[isodd.(A)]
-    @test A[Not([])] == A[collect(1:end)] == collect(A)
+    @test A[Not([])] == A[collect(1:end)] == collect(A[:])
 end
 
 @testset "vector array" begin

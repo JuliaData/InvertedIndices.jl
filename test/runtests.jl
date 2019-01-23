24c68e753985c429d3d04b22a78d99445d9ea84e
Matt Bauman <mbauman@gmail.com>
2019-01-23 03:13:25 -0600

    Major refactor and update for 1.0
    Use a new lazy iterator forumalation to avoid excessive allocations (a la Base.LogicalIndex). Fixes #2 and should fix #1 (although I've not run any benchmarks yet).

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,11 +4,13 @@ using OffsetArrays
 
 @testset "0-d" begin
     A = fill(1)
-    @test A[Not(A.==1)] == []
+    @test A[Not(fill(A.==1))] == []
     @test A[Not(CartesianIndex())] == []
-    @test A[Not(A.==2)] == [1]
-    A[Not(A.==2)] = 0
+    @test A[Not(fill(A.==2))] == [1]
+    A[Not(fill(A.==2))] = fill(0)
     @test A[] == 0
+    A[Not(fill(A.==2))] .= 1
+    @test A[] == 1
 end
 
 @testset "1-d readonly" for A in (-10:13, reshape(-10:13,2,:), reshape(-10:13,3,2,:))
@@ -27,8 +29,8 @@ end
 end
 
 @testset "1-d offset readonly" for A in (OffsetArray(-10:13, -3), OffsetArray(reshape(-10:13,2,:), -5, 32), OffsetArray(reshape(-10:13,3,2,:), 4,-2,20))
-    f = first(linearindices(A))
-    l = last(linearindices(A))
+    f = first(LinearIndices(A))
+    l = last(LinearIndices(A))
     @test A[Not(f)] == A[Not(f:f)] == A[Not(A.==-10)] == collect(-9:13)
     @test @views A[Not(f)] == A[Not(f:f)] == A[Not(A.==-10)] == collect(-9:13)
     @test A[Not(l)] == A[Not(l:l)] == A[Not(A.==13)] == collect(-10:12)
@@ -44,14 +46,14 @@ end
 @testset "1-d read/write" for A in (collect(1:4), reshape(collect(1:4),2,2))
     A[Not(2:3)] = [44, 11]
     @test vec(A) == [44, 2, 3, 11]
-    A[Not(2:4)] = 0
+    A[Not(2:4)] .= 0
     @test vec(A) == [0, 2, 3, 11]
-    A[Not(1:end)] = 100
+    A[Not(1:end)] .= 100
     @test vec(A) == [0, 2, 3, 11]
-    A[Not(:)] = 100
+    A[Not(:)] .= 100
     @test vec(A) == [0, 2, 3, 11]
 
-    A[Not([])] = 0
+    A[Not([])] .= 0
     @test all(A.== 0)
 
     B = copy(A)
@@ -60,18 +62,18 @@ end
     @test A == B
 end
 
-@testset "2-d readonly" for A in (reshape(-10:13,3,:), reshape(-10:13,3,4,:))
+@testset "2-d readonly" for A in (reshape(-10:13,3,:), reshape(-10:13,3,:,1))
     @test A[Not(2), :] == (@view A[Not(2), :]) == A[[1,3],:]
     @test A[:, Not(2)] == (@view A[:, Not(2)]) == A[:,[1;3:end]]
     @test A[Not(2), Not(2)] == (@view A[Not(2), Not(2)]) == A[[1;3:end],[1;3:end]]
-    R = collect(CartesianRange(size(A)))
+    R = collect(CartesianIndices(size(A)))
     @test A[Not(first(R))] == (@view A[Not(first(R))]) == A[2:end]
     @test A[Not(R[1:2])] == (@view A[Not(R[1:2])]) == A[3:end]
     @test A[Not(iseven.(A))] == (@view A[Not(iseven.(A))]) == A[isodd.(A)] == collect(-9:2:13)
 end
 
 @testset "2-d offset readonly" for A in (OffsetArray(reshape(-10:13,3,:), -1, 0), OffsetArray(reshape(-10:13,3,4,:), -10, 20, -30),)
-    inds = indices(A)
+    inds = axes(A)
     f₁, l₁ = first(inds[1]), last(inds[1])
     f₂, l₂ = first(inds[2]), last(inds[2])
     # TODO: Re-enable these tests for 3-d after PLI deprecation
@@ -80,8 +82,8 @@ end
         @test A[f₁:l₁, Not(f₂+1)] == (@view A[f₁:l₁, Not(f₂+1)]) == A[f₁:l₁,[f₂;f₂+2:l₂]]
         @test A[Not(f₁+1), Not(f₂+1)] == (@view A[Not(f₁+1), Not(f₂+1)]) == A[[f₁,l₁],[f₂;f₂+2:l₂]]
     end
-    R = collect(CartesianRange(indices(A)))
-    @test A[Not(first(R))] == (@view A[Not(first(R))]) == A[linearindices(A)[2:end]]
-    @test A[Not(R[1:2])] == (@view A[Not(R[1:2])]) == A[linearindices(A)[3:end]]
+    R = collect(CartesianIndices(axes(A)))
+    @test A[Not(first(R))] == (@view A[Not(first(R))]) == A[LinearIndices(A)[2:end]]
+    @test A[Not(R[1:2])] == (@view A[Not(R[1:2])]) == A[LinearIndices(A)[3:end]]
     @test A[Not(iseven.(A))] == (@view A[Not(iseven.(A))]) == A[isodd.(A)] == collect(-9:2:13)
 end
